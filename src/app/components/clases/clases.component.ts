e6ecfd020e7234a8edf6b981d4f614aec947eec9
Garavirod <rodrigogarciaavila26@gmail.com>
2020-01-05 19:17:05 -0600

    Proceso asíncrono

--- src/app/components/clases/clases.component.ts
+++ src/app/components/clases/clases.component.ts
@@ -7,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClasesComponent implements OnInit {
   alerta: string = 'alert-danger';
+  loading: boolean = false;
   property: Object = {
     danger: false
   }
   constructor() { }
   ngOnInit() {
+  }
+
+  executeProcess(){
+    this.loading = true;
+    // After happening 3 secons the value of 'loading' will change 
+    setTimeout( () => this.loading = false, 3000);
   }
 
 }
