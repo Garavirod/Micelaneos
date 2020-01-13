ad7e222a1ebfdeefec9f3e58027fc072be71b356
Garavirod <rodrigogarciaavila26@gmail.com>
2020-01-12 21:43:54 -0600

    Ciclo de vida de componentes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
+AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
+// tslint:disable-next-line: no-conflicting-lifecycle
 @Component({
   selector: 'app-home',
   template: `
@@ -11,9 +13,7 @@ import { Component, OnInit } from '@angular/core';
         Hellow from app-componet
     </p>
     <app-clases></app-clases>
-
     <!-- Directivas personalizadas -->
-
     <br><br>
     <h3>Directivas personalizadas</h3>
 
@@ -24,8 +24,6 @@ import { Component, OnInit } from '@angular/core';
     <p appResaltado>
         this is using appResaltado directive too. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi numquam quia perferendis natus nobis dolorem molestiae sapiente sint ea inventore!
     </p>
-
-
     <!-- NG-SWITCH -->
     <br><br>
     <h3>Ng-Switch</h3>
@@ -34,11 +32,43 @@ import { Component, OnInit } from '@angular/core';
   `,
   styles: []
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
+AfterViewInit, AfterViewChecked, OnDestroy{
 
-  constructor() { }
+  constructor() { 
+    console.log('Constructor');
+  }
 
   ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges(){
+    console.log('ngOnChanges');
+  }
+
+  ngDoCheck(){
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy');
   }
 
 }
